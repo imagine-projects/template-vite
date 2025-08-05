16cb0d5fa6c713b9b05772b5c44a1b8506c7c623
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-08-05 16:58:54 -0500

    remove redundant stuff

--- base-nextjs-template/app/page.tsx
+++ base-nextjs-template/app/page.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
   return (
-    <div className="font-sans">
-      <div className="min-h-screen flex flex-col">
-        <div className="flex-grow flex justify-center items-center text-3xl">
-          <h1>Welcome to your Imagine project</h1>
-        </div>
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow flex justify-center items-center text-3xl">
+        <h1>Welcome to your Imagine project</h1>
       </div>
     </div>
   );
