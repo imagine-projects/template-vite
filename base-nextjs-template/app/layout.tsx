4fb958927ea1249363f80c3314fe7160fa236fce
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-08-05 16:52:32 -0500

    use inter font

--- base-nextjs-template/app/layout.tsx
+++ base-nextjs-template/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistSans = Inter({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
