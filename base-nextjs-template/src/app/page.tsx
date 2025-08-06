c25205f391ead4eeb2cb6c52509b6a775157bd84
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-08-06 10:22:00 -0500

    remove nextjs for now

--- base-nextjs-template/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex-grow flex justify-center items-center text-3xl">
-        <h1>Welcome to your Imagine project</h1>
-      </div>
-    </div>
-  );
-}
