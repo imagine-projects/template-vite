bebc379d493ca7fce4aafafc727dd37b7c75560c
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-08-06 14:42:00 -0500

    remove embedded via searchparams

--- base-vite-template/src/routes/__root.tsx
+++ base-vite-template/src/routes/__root.tsx
@@ -2,9 +2,6 @@ import { RouterContext } from "@/main";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-const currentUrl = new URL(window.location.href);
-const isEmbedded = currentUrl.searchParams.get("embedded") === "true";
-
 export const Route = createRootRouteWithContext<RouterContext>()({
   beforeLoad: async ({ location }) => {
     console.log("beforeLoad __root", location.pathname);
@@ -12,7 +9,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <Outlet />
-      {!isEmbedded && <TanStackRouterDevtools />}
+      <TanStackRouterDevtools />
     </>
   ),
 });
